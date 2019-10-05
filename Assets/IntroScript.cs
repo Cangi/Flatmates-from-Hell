7bcd47d8b8b67ab9e7d1bdd1bb9eba93d7d5f812
Paulius Kuzmickas <p.kuzmickas@gmail.com>
2019-10-06 00:16:57 +0100

    Added microwave/intro

---
+++ Assets/IntroScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroScript : MonoBehaviour
+{
+    public Sprite secondImage;
+    private bool secondStage;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!secondStage)
+            {
+                secondStage = true;
+                GetComponent<Image>().sprite = secondImage;
+            }
+            else
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+}
