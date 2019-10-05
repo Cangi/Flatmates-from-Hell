9ff8c0e759fbab86b48e82de335fe69c50d9cb55
Cangi <rony@xrealitylabs.com>
2019-10-05 20:59:32 +0100

    Improved the GUI

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    public TextMeshProUGUI cleanLevelText;
+
+    public float time = 300f;
+
+    public int minutes;
+
+    public int seconds;
+
+    public int cleanLevel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        minutes = Mathf.RoundToInt(time / 60) - 1;
+        seconds = Mathf.RoundToInt(time % 60);
+
+        timerText.text = "Time: " + minutes + ":" + seconds;
+        cleanLevelText.text = "Clean Level: " + cleanLevel;
+
+    }
+}
