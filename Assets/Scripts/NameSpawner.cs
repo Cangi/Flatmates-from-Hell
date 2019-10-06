c79314bde2d3c4bdab5caa6d8738a9fba75eb81d
AstryX <robertasdere@gmail.com>
2019-10-06 17:36:29 +0100

    Added dynamic text to adversaries

---
+++ Assets/Scripts/NameSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameSpawner : MonoBehaviour
+{
+
+    private TextMeshProUGUI nameObject;
+
+    public string charName;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        NameManager nameMan = GameObject.Find("NameManager").GetComponent(typeof(NameManager)) as NameManager;
+        charName = nameMan.retrieveName();
+        nameObject = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+        nameObject.text = charName;
+    }
+}
