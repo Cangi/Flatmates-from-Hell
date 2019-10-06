275cfd0fc521be6548d832bacc0bf5cebe1a905d
Paulius Kuzmickas <p.kuzmickas@gmail.com>
2019-10-06 14:22:41 +0100

    NEWEST BUILD

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Sprite doorOpened;
+
+    public Sprite doorClosed;
+    private SpriteRenderer spriteR;
+    private bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        isOpen = false;
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        Debug.Log("entered trigger!");
+        if (!isOpen)
+        {
+            spriteR.sprite = doorOpened;
+            isOpen = true;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (isOpen)
+        {
+            spriteR.sprite = doorClosed;
+            isOpen = false;
+        }
+    }
+}
